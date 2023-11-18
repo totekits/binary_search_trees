5b7439670d31cc78ee9280cb007cde0ae168d370
totekits <tote.kits@gmail.com>
2023-11-18 16:10:22 +0700

    Complete the code

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,15 @@
 class Node
   include Comparable
+  attr_accessor :data, :left, :right
 
-  def initialize(value, left=nil, right=nil)
-    @value = value
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
     @left = left
     @right = right
+  end
+
+  def <=>(other)
+    value = other.class == Node ? other.data : other
+    data <=> value
   end
 end
