4f15dc64a899272bf64ba40766ad31e8e4ba58d9
totekits <tote.kits@gmail.com>
2023-11-22 13:14:01 +0700

    Edit script

--- main.rb
+++ main.rb
@@ -1,19 +1,12 @@
 require_relative 'lib/tree'
 
-tree = build_tree(Array.new(15) { rand(1..100) })
-pretty_print(tree.root)
+tree = Tree.new(Array.new(15) { rand(1..100) })
 puts "The tree is balanced: #{tree.balance?}"
-puts "Level order: #{level_order(tree.root)}"
-puts "Preorder: #{preorder(tree.root)}"
-puts "Postorder: #{postorder(tree.root)}"
-puts "Inorder: #{inorder(tree.root)}"
+puts "Level order: #{tree.level_order}"
+puts "Preorder: #{tree.preorder}"
+puts "Postorder: #{tree.postorder}"
+puts "Inorder: #{tree.inorder}"
 tree.insert(111)
 tree.insert(222)
 tree.insert(333)
 puts "The tree is balanced after adding heavy numbers: #{tree.balance?}"
-rebalance(tree)
-puts "The tree is balanced after rebalance method was called: #{tree.balance?}"
-puts "Level order: #{level_order(tree.root)}"
-puts "Preorder: #{preorder(tree.root)}"
-puts "Postorder: #{postorder(tree.root)}"
-puts "Inorder: #{inorder(tree.root)}"
